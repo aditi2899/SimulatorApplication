aa81f5f4e367a74adf06b4b57ca94e465941e00d
Aditi <asinghal1@ee.iitr.ac.in>
2022-04-03 23:41:53 +0530

    initial setup

---
+++ app/src/main/java/com/example/simulatorapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.simulatorapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn1 = findViewById(R.id.button1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(getApplicationContext(),Form1.class);
+                startActivity(i);
+            }
+        });
+    }
+
+}
