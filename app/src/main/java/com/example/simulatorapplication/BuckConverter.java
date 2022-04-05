e5263dcfc0d25c7fd136da2edb92726493b026bc
ildwi <68910831+ildwi@users.noreply.github.com>
2022-04-06 03:55:03 +0530

    buck_aditi

--- app/src/main/java/com/example/simulatorapplication/BuckConverter.java
+++ app/src/main/java/com/example/simulatorapplication/BuckConverter.java
@@ -2,7 +2,11 @@ package com.example.simulatorapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class BuckConverter extends AppCompatActivity {
 
@@ -10,5 +14,44 @@ public class BuckConverter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buckconverter);
+
+        EditText Volt = (EditText) findViewById(R.id.voltage);
+        int V = Integer.parseInt(Volt.getText().toString());
+
+        EditText res = (EditText) findViewById(R.id.resistance);
+        int Res = Integer.parseInt(res.getText().toString());
+
+        EditText Cap = (EditText) findViewById(R.id.capacitance);
+        int C = Integer.parseInt(Cap.getText().toString());
+
+        EditText Ind = (EditText) findViewById(R.id.inductance);
+        int I = Integer.parseInt(Ind.getText().toString());
+
+        EditText On = (EditText) findViewById(R.id.t_on);
+        int T_On = Integer.parseInt(On.getText().toString());
+
+        EditText Off = (EditText) findViewById(R.id.t_off);
+        int T_Off = Integer.parseInt(Off.getText().toString());
+
+        EditText vc_i = (EditText) findViewById(R.id.vc_initial);
+        int Vc_initial = Integer.parseInt(vc_i.getText().toString());
+
+        EditText il_i = (EditText) findViewById(R.id.il_initial);
+        int Il_initial = Integer.parseInt(il_i.getText().toString());
+
+        EditText tot = (EditText) findViewById(R.id.t_tot);
+        int T_tot = Integer.parseInt(tot.getText().toString());
+
+
+        Button btn = findViewById(R.id.submit);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(getApplicationContext(), resultBuck.class);
+                startActivity(i);
+            }
+        });
     }
+
 }
